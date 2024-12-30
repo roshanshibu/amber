bfaa30ab539981a500765b2d5860e9db1d5666b3
Roshan Shibu <roshanshibu97@gmail.com>
2024-12-30 15:52:49 +0100

    extract tags, prepare files for streaming and move to Music folder

--- Scripts/app.py
+++ Scripts/app.py
@@ -1,9 +1,10 @@
-import glob
 import os
+from pathlib import Path
 from music_analysis import analyse_song
-from utils import get_uuid
+from utils import get_mp3_tags, get_uuid
 from config import STAGING_DIR, MUSIC_DIR, valid_extensions
 import hashlib
+from PIL import Image
 
 
 def init():
@@ -25,7 +26,7 @@ def update_library():
 
     # for each file in the list of acceptable formats inside Staging...
     for file in staging_files:
-        print(file)
+        print(f"\n{file} {'-' * 80}")
         file_path = STAGING_DIR / file
 
         # compute the sha256 hash
@@ -38,15 +39,33 @@ def update_library():
         song_uuid = get_uuid()
 
         # analyze the song for features
-        song_features = analyse_song(file_path)
+        # song_features = analyse_song(file_path)
 
-        # TODO: extract song name if possible from mp3 tags, otherwise use the filename
-        # TODO: extract all song metadata: artists, album, album art, release dates etc.
+        # extract all song metadata: song name (title), artists, album & album art
+        tags = get_mp3_tags(file_path)
+
         # TODO: push all information to the DB (including the hash)
-        # TODO: create a folder with name as GUID
-        # TODO: rename song file - add GUID to the beginning
-        # TODO: move the song file to the GUID folder
-        # TODO: run ffmpeg and split the file for HSL steaming purposes
+
+        # create a folder with name as song_uuid in Music folder
+        song_folder = MUSIC_DIR / song_uuid
+        Path(song_folder).mkdir(parents=True, exist_ok=False)
+
+        # rename song file - add song_uuid to the beginning, and move it to the song_uuid folder
+        os.rename(file_path, song_folder / file)
+
+        # if album art exists, write it to uuid.png
+        if tags["cover_art"]:
+            try:
+                cover_art = Image.open(tags["cover_art"])
+            except Exception as e:
+                return
+
+            # Save as PNG, ignoring original image format
+            cover_art.save(song_folder / f"{song_uuid}.png", format="PNG")
+
+        # run ffmpeg and split the file for HSL steaming purposes
+        hls_split_command = f'ffmpeg -i "{song_folder / file}" -map 0:a -c:a aac -b:a 128k -hls_time 5 -hls_list_size 0 -hls_segment_filename {song_folder / song_uuid}_%03d.ts -f hls {song_folder / song_uuid}.m3u8'
+        os.system(hls_split_command)
 
 
 init()
